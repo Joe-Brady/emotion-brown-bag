2680b5211c10bc0fd769c667d5f0d5f3a15cdc42
Joe Brady <joe.brady@uk.ibm.com>
2018-08-15 14:37:33 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Emotion styling demo</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
